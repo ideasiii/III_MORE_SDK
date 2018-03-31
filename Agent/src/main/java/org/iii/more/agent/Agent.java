d3b71d95b6439821789211ad614f7ff35551e45c
ideasiii <louisju@iii.org.tw>
2018-03-31 17:10:21 +0800

    jugo update

--- Agent/src/main/java/org/iii/more/agent/Agent.java
+++ Agent/src/main/java/org/iii/more/agent/Agent.java
@@ -6,6 +6,7 @@ import org.iii.more.restapiclient.Response;
 import org.iii.more.restapiclient.RestApiClient;
 import org.json.JSONObject;
 
+import java.net.HttpURLConnection;
 import java.util.HashMap;
 
 /**
@@ -15,8 +16,8 @@ public abstract class Agent
 {
     private static RestApiClient restApiClient = new RestApiClient();
     private static SdkData sdkData = new SdkData();
-    private static final String mstrURL = "http://192.168.0.105/api/test.jsp"; //https://ryejuice
-    // .sytes.net/api/test.jsp
+    private static final String mstrURL = "http://192.168.0.105/api/sdkSync.jsp";
+    
     public static synchronized void syncData()
     {
         restApiClient.setResponseListener(responseListener);
@@ -24,18 +25,36 @@ public abstract class Agent
         HashMap<String, String> param = new HashMap<String, String>();
         param.put("1", "a");
         Response response = new Response();
-        int nResponse_id = restApiClient.HttpPost(mstrURL,
-                Config.HTTP_DATA_TYPE.X_WWW_FORM, param, response);
+        int nResponse_id = restApiClient.HttpPost(mstrURL, Config.HTTP_DATA_TYPE.X_WWW_FORM, param, response);
         Logs.showTrace("[Agent] https response id: " + nResponse_id);
     }
     
-    private static RestApiClient.ResponseListener responseListener = new RestApiClient
-            .ResponseListener()
+    private static RestApiClient.ResponseListener responseListener = new RestApiClient.ResponseListener()
     {
         @Override
         public void onResponse(JSONObject jsonObject)
         {
-            Logs.showTrace("[Agent] onResponse Data: " + jsonObject.toString());
+            try
+            {
+                if (null != jsonObject)
+                {
+                    if (!jsonObject.isNull("code"))
+                    {
+                        if (HttpURLConnection.HTTP_OK == jsonObject.getInt("code"))
+                        {
+                            if (!jsonObject.isNull("data"))
+                            {
+                                JSONObject jsonData = new JSONObject(jsonObject.getString("data"));
+                                Logs.showTrace("[Agent] onResponse Data: " + jsonData);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Logs.showError("[Agent] responseListener Exception: " + e.getMessage());
+            }
         }
     };
 }
